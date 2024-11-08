7d010fc2d39de6107d1a0d0210c99b3198c25acb
Thejus Thomson <thomson.th@northeastern.edu>
2024-11-08 13:52:30 -0500

    revamping project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/ProjectCardStyle.css";
-import {FaArrowRight} from "react-icons/fa";
-import {FiGithub} from "react-icons/fi";
-// import Tilt from 'react-parallax-tilt';
+import { FaArrowRight, FaChevronDown } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 export default function ProjectCard(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const handleOnClick = () => {
     window.open(`${props.projectLink}`);
   }
 
   return (
     <div className='project-window' id={props.id}>
-      <div className={`project-wrapper ${props.className}`}>
+      <div className={`project-wrapper ${props.className} ${isExpanded ? 'expanded' : ''}`}>
         <div className="about-project">
-          <h2 className="project-title">{props.projectTitle}</h2>
-          <div className="desc">{props.projectDesc}</div>
+          <div className="project-header">
+            <h2 className="project-title">{props.projectTitle}</h2>
+            <button 
+              className={`expand-toggle ${isExpanded ? 'expanded' : ''}`}
+              onClick={() => setIsExpanded(!isExpanded)}
+            >
+              <FaChevronDown />
+            </button>
+          </div>
+          <div className="project-content">
+            <div className="project-preview">
+              {props.shortDesc || props.projectDesc.substring(0, 100) + '...'}
+            </div>
+            <div className="project-full">
+              {props.projectDesc}
+            </div>
+          </div>
           <button className='btn' onClick={handleOnClick}>
             <span>View on GitHub</span>
             <FiGithub className="social" size={20}/>
